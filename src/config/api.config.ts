0809335c4f1b62dd9081d2332cfce65325bb4282
YaCn Naghibi <yasin.naghibi9@gmail.com>
2023-08-10 02:33:14 +0330

    fix: config issue fixed

--- src/config/api.config.ts
+++ src/config/api.config.ts
@@ -2,5 +2,5 @@ const isProduction = process.env.NODE_ENV === "production";
 
 export const BASE_URL =
     process.env.REACT_APP_API_BASE_URL || isProduction
-        ? "https://todo-app-vercel-iota.vercel.app"
+        ? "https://todo-server-eosin.vercel.app"
         : "http://localhost:8182";
